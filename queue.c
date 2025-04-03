9d242ddec1c7bae7e9e7ebc113591e115f393f8b
jonatan lund <jonathanslund1@gmail.com>
2025-04-03 18:14:32 -0400

    Final solution (to slow to get 3 of the test cases)

--- queue.c
+++ queue.c
@@ -1,6 +1,7 @@
 #include "queue.h"
 #include "tile_game.h"
 #include <stdlib.h>
+#include <time.h>
 
 void enqueue(struct queue *q, struct game_state state) {
     //Serializing game state
@@ -25,6 +26,7 @@ void print_game_state(struct game_state state) {
     fprintf(stderr,"num_steps: %d\n",state.num_steps);
 }
 
+/*
 struct game_state copy_state(struct game_state state) {
     struct game_state copy;
     copy.num_steps = state.num_steps;
@@ -37,8 +39,10 @@ struct game_state copy_state(struct game_state state) {
     copy.empty_row = state.empty_row;
     return copy;
 }
+*/
 
 int final_state(struct game_state state) {
+    if (state.empty_col != 3 || state.empty_row != 3) return false;
     for (uint8_t i = 0; i < 4; i++) {
         for (uint8_t j = 0; j < 4; j++) {
             if (i == 3 && j == 3) continue;
@@ -49,6 +53,7 @@ int final_state(struct game_state state) {
 }
 
 int number_of_moves(struct game_state start) {
+    clock_t start_time = clock();
     //Initializing queue
     struct linked_list * queue_list = malloc(sizeof(struct linked_list));
     queue_list -> head = NULL;
@@ -62,8 +67,11 @@ int number_of_moves(struct game_state start) {
     int num_moves = -1;
     while (true)
     {
+        double seconds = (double) (clock()-start_time)/CLOCKS_PER_SEC;
+        if (seconds > 50) break;
         //Popping current state
         struct game_state curr = dequeue(state_queue);
+        uint64_t serial_curr = serialize(curr);
         //Testing if we have found the solution
         if (final_state(curr))
         {
@@ -72,19 +80,19 @@ int number_of_moves(struct game_state start) {
         }
         
         //Applying moves to state
-        struct game_state up = copy_state(curr);
+        struct game_state up = deserialize(serial_curr);
         move_up(&up);
-        struct game_state down = copy_state(curr);
+        struct game_state down = deserialize(serial_curr);
         move_down(&down);
-        struct game_state left = copy_state(curr);
+        struct game_state left = deserialize(serial_curr);
         move_left(&left);
-        struct game_state right = copy_state(curr);
+        struct game_state right = deserialize(serial_curr);
         move_right(&right);
         //Queueing new states
-        if (serialize(curr) != serialize(up)) enqueue(state_queue,up);
-        if (serialize(curr) != serialize(down)) enqueue(state_queue,down);
-        if (serialize(curr) != serialize(left)) enqueue(state_queue,left);
-        if (serialize(curr) != serialize(right)) enqueue(state_queue,right);
+        if (serial_curr != serialize(up)) enqueue(state_queue,up);
+        if (serial_curr != serialize(down)) enqueue(state_queue,down);
+        if (serial_curr != serialize(left)) enqueue(state_queue,left);
+        if (serial_curr != serialize(right)) enqueue(state_queue,right);
     }
 
     //Freeing used structures
